63d9d769d21411b6eead9bd399a01926f1d1c514
tran thanh tu <tttuab@gmail.com>
2022-11-24 22:43:50 +0700

    update

--- routes/food.route.js
+++ routes/food.route.js
@@ -1,11 +1,16 @@
 var express = require("express");
-const { getOneFoodById } = require("../services/food.service");
+const { getOneFoodById, getAllFood } = require("../services/food.service");
 
 var router = express.Router();
 
 router.get("/:foodId", async (req, res) => {
   let foodId = req?.params?.foodId;
   let response = await getOneFoodById(foodId);
+  res.json(response);
+});
+
+router.get("/", async (req, res) => {
+  let response = await getAllFood();
   res.json(response);
 });
 
--- services/food.service.js
+++ services/food.service.js
@@ -27,4 +27,32 @@ const getOneFoodById = async (foodId) => {
   }
 };
 
-module.exports = { getOneFoodById };
+const getAllFood = async () => {
+  try {
+    let foods = await MongoDB.db
+      .collection(mongoConfig.collections.FOODS)
+      .find()
+      .toArray();
+    if (foods) {
+      return {
+        status: true,
+        message: "Food found successfully",
+        data: foods,
+      };
+    } else {
+      return {
+        status: false,
+        message: "No Food found",
+      };
+    }
+  } catch (error) {
+    return {
+      status: false,
+      message: "Food finding failed",
+      error: `Food finding failed : ${error?.message}`,
+    };
+  }
+};
+
+
+module.exports = { getOneFoodById ,getAllFood};
